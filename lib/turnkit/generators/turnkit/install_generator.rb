4b48ff01a3c6a6859deab63d0f24c812870eb588
Sam Couch <sam@samcouch.com>
2026-06-04 13:36:31 -0400

    Fix install generator migration compatibility

--- lib/turnkit/generators/turnkit/install_generator.rb
+++ lib/turnkit/generators/turnkit/install_generator.rb
@@ -8,6 +8,8 @@ module TurnKit
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
 
+      namespace "turnkit:install"
+
       source_root File.expand_path("install/templates", __dir__)
 
       class_option :table_prefix, type: :string, default: "turnkit", desc: "Database table prefix."
@@ -28,10 +30,18 @@ module TurnKit
       end
 
       def self.next_migration_number(dirname)
-        if ActiveRecord::Base.timestamped_migrations
+        if timestamped_migrations?
           Time.now.utc.strftime("%Y%m%d%H%M%S")
         else
           "%.3d" % (current_migration_number(dirname) + 1)
+        end
+      end
+
+      def self.timestamped_migrations?
+        if ActiveRecord.respond_to?(:timestamped_migrations)
+          ActiveRecord.timestamped_migrations
+        else
+          ActiveRecord::Base.timestamped_migrations
         end
       end
 
